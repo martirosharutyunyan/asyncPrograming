21061c869fae02e1050fd66913acc91dbf164060
martiros888 <harutunyan.martiros@mail.ru>
2021-06-11 20:03:44 +0400

    add

--- toAsync.js
+++ toAsync.js
@@ -2,7 +2,6 @@ const count = (a, b) => a + b;
 
 const toAsync = fn => (...args) => new Promise((res, rej) => res(fn(...args)));
 
+const count2 = toAsync(count);
 
-const count2 = toAsync(count)
-
-count2(1, 2).then(res => console.log(res))
+count2(1, 2).then(console.log);
